a4a9033478bdd1a57f4ecf3dd33a4f7babcde489
vinodell <max.khamitov@mail.ru>
2020-10-05 17:19:42 +0300

    week13-1

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import Header from './header'
+import First from './first'
+import Repo from './repo'
+import Text from './text'
 
 const Home = () => {
   return (
@@ -7,7 +11,11 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          {/* add your routes here */}
+          <Switch>
+            <Route exact path="/first/" component={() => <First />} />
+            <Route exact path="/first/:username" component={() => <Repo />} />
+            <Route exact path="/first/:username/:text" component={() => <Text />} />
+          </Switch>
         </div>
       </div>
     </div>
